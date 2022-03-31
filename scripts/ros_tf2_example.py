d9a95bf9fb4eaf45f3c08ac9de540dad5cd5ffb9
hsaeid-uncw <saeidih@uncw.edu>
2022-03-31 14:40:32 +0000

    point conversion

--- scripts/ros_tf2_example.py
+++ scripts/ros_tf2_example.py
@@ -4,6 +4,7 @@ import rospy
 import tf2_ros
 from tf.transformations import *
 from geometry_msgs.msg import Quaternion
+import tf2_geometry_msgs
 
 
 if __name__ == '__main__':
@@ -33,8 +34,18 @@ if __name__ == '__main__':
 		q_rot = trans.transform.rotation
 		roll, pitch, yaw, = euler_from_quaternion([q_rot.x, q_rot.y, q_rot.z, q_rot.w])
 		# a quick check of the readings
-		print('x: ', format(x, '.3f'), ', y: ', format(y, '.3f'), ', z: ', format(z, '.3f'))
-		print('roll: ', format(roll, '.2f'), ', pitch: ', format(pitch, '.2f'), ', yaw: ', format(yaw, '.2f')) 
+		print('Tool frame position and orientation w.r.t base: x= ', format(x, '.3f'), '(m),  y= ', format(y, '.3f'), '(m), z= ', format(z, '.3f'),'(m)')
+		print('roll= ', format(roll, '.2f'), '(rad), pitch= ', format(pitch, '.2f'), '(rad), yaw: ', format(yaw, '.2f'),'(rad)') 
+		# define a testpoint in the tool frame (let's say 10 cm away from flange)
+		pt_in_tool = tf2_geometry_msgs.PointStamped()
+		pt_in_tool.header.frame_id = 'fk_tooltip'
+		pt_in_tool.header.stamp = rospy.get_rostime()
+		pt_in_tool.point.z= 0.1 # 10 cm away from flange
+		# conert the 3D point to the base frame coordinates
+		pt_in_base = tfBuffer.transform(pt_in_tool,'base', rospy.Duration(1.0))
+		print('Test point in the TOOL frame:  x= ', format(pt_in_tool.point.x, '.3f'), '(m), y= ', format(pt_in_tool.point.y, '.3f'), '(m), z= ', format(pt_in_tool.point.z, '.3f'),'(m)')
+		print('Transformed point in the BASE frame:  x= ', format(pt_in_base.point.x, '.3f'), '(m), y= ', format(pt_in_base.point.y, '.3f'), '(m), z= ', format(pt_in_base.point.z, '.3f'),'(m)')
+		print('-------------------------------------------------')
 		# wait for 0.1 seconds until the next loop and repeat
 		loop_rate.sleep()
 
